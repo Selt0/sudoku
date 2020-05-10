8c9954857d44e7dfde11bd4262cb64f6beedd0d4
Selt0 <chewedgum12@gmail.com>
2020-05-10 13:48:23 -0500

    Add method to determine rows, cols, squares, and change values

--- board.rb
+++ board.rb
@@ -1,6 +1,7 @@
 require_relative 'tile'
 
 class Board
+
   def self.parse_file(filename)
     rows = File.readlines(filename).map(&:chomp)
 
@@ -16,4 +17,45 @@ class Board
   def initialize(board)
     @board = board
   end
+
+  def [](pos)
+    x, y = pos
+    board[x][y]
+  end
+
+  def []=(pos, value)
+    x, y = pos
+    tile = board[x][y]
+    tile.value = value
+  end
+
+  def rows
+    board
+  end
+
+  def cols
+    rows.transpose
+  end
+
+  def squares
+    (0..8).to_a.map{ |i| square(i) }
+  end
+
+  def square(idx)
+    tiles = []
+    x = (idx / 3) * 3
+    y = (idx % 3) * 3
+
+    (x...x + 3).each do |i|
+      (y...y + 3).each do |j|
+        tiles << self[[i, j]]
+      end
+    end
+
+    tiles
+  end
+
+  private
+
+  attr_reader :board
 end
