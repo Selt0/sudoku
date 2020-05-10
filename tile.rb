cc3730da2e94da3bdfb603c0ca21fcc1b93ef9cc
Selt0 <chewedgum12@gmail.com>
2020-05-10 13:32:05 -0500

    Rename class. Fix syntax error

---
+++ tile.rb
@@ -0,0 +1,30 @@
+require 'colorize'
+
+class Tile
+  attr_reader :value
+
+  def initialize(value)
+    @value = value
+     @given = value == 0 ? false : true
+  end
+
+  def given?
+    @given
+  end
+
+  def to_s
+    value == 0 ?  ' ' : value.to_s.colorize(color)
+  end
+
+  def value=(new_value)
+    if given?
+      puts "You can't change that value"
+    else
+      @value = new_value
+    end
+  end
+
+  def color
+    given? ? :orange : :white
+  end
+end
